b7c19a1e2a227059c56c28733a3d8010371b0105
Vladymyr Nazarenko <vpnazarenko@gmail.com>
2021-08-09 20:40:53 +0300

    1

---
+++ index.js
@@ -0,0 +1,37 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const startBtnLink = document.querySelector('[data-action="start"]');
+const stoptBtnLink = document.querySelector('[data-action="stop"]');
+const bodyLink = document.querySelector("body");
+let runActionId = null;
+
+startBtnLink.addEventListener("click", randomChangeBodyColor);
+stoptBtnLink.addEventListener("click", stopChangeBodyColor);
+
+function randomChangeBodyColor(e) {
+  bodyLink.setAttribute("style", "background");
+
+  runActionId = setInterval(() => {
+    if (e) {
+      startBtnLink.disabled = true;
+    }
+    const randomIntegerFromInterval = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    let randNum = randomIntegerFromInterval(0, 5);
+    bodyLink.style.background = `${colors[randNum]}`;
+    startBtnLink.backgroundColor = `${colors[randNum]}`;
+  }, 1000);
+}
+function stopChangeBodyColor() {
+  clearInterval(runActionId);
+  startBtnLink.disabled = false;
+}

df78275568ee25fe311df8486acea64976ffe4d8
Vladymyr Nazarenko <vpnazarenko@gmail.com>
2021-08-16 19:49:10 +0300

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
-const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548",
-];
+const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
 
 const startBtnLink = document.querySelector('[data-action="start"]');
 const stoptBtnLink = document.querySelector('[data-action="stop"]');
-const bodyLink = document.querySelector("body");
+const bodyLink = document.querySelector('body');
 let runActionId = null;
 
-startBtnLink.addEventListener("click", randomChangeBodyColor);
-stoptBtnLink.addEventListener("click", stopChangeBodyColor);
+startBtnLink.addEventListener('click', randomChangeBodyColor);
+stoptBtnLink.addEventListener('click', stopChangeBodyColor);
 
 function randomChangeBodyColor(e) {
-  bodyLink.setAttribute("style", "background");
+  bodyLink.setAttribute('style', 'background');
+  startBtnLink.disabled = true;
 
   runActionId = setInterval(() => {
-    if (e) {
-      startBtnLink.disabled = true;
-    }
     const randomIntegerFromInterval = (min, max) => {
       return Math.floor(Math.random() * (max - min + 1) + min);
     };
 
-    let randNum = randomIntegerFromInterval(0, 5);
+    let randNum = randomIntegerFromInterval(0, colors.length);
     bodyLink.style.background = `${colors[randNum]}`;
     startBtnLink.backgroundColor = `${colors[randNum]}`;
   }, 1000);
